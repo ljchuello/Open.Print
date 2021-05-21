f376e8217005b02086409bae9464b1ad27194e08
Leonardo Chuello <ljchuello@gmail.com>
2021-05-21 16:44:05 -0500

    Ya hace impresión

--- Test01/Default.aspx.cs
+++ Test01/Default.aspx.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using Codaxy.WkHtmlToPdf;
 
 namespace Test01
 {
-	public partial class Default : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime a = DateTime.Now;
+                PdfConvert.ConvertHtmlToPdf(new PdfDocument
+                {
+                    Url = "https://github.com/"
 
-		}
-	}
+                }, new PdfOutput
+                {
+                    OutputFilePath = $"D:\\pdf\\{Guid.NewGuid()}.pdf"
+                });
+                DateTime b = DateTime.Now;
+
+                var diferencia = b - a;
+                Label1.Text = $"{diferencia.TotalMilliseconds:n2}";
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
 }
